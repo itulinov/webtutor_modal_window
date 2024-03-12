b557c1d635737648fb7e0c3387a099301679bdcf
iTulinov <energiay@gmail.com>
2024-03-12 18:20:21 +0300

    импорт для прод\дев режимов

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,11 @@
 function App({settings}) {
     console.log("settings: ", settings)
-    import ("./modal_window.js")
+
+    import ("./modal_window_lib.js").catch((err) => {
+        return import("webtutor_modal_window_lib")
+    }).then(({default: run}) => {
+
+    })
 }
 
 export default App
---
+++ src/App/modal_window_lib.js
@@ -0,0 +1,16 @@
+import mfLib from "mwLib/mwLibInit"
+export default mfLib
+
+//import npmLib from "webtutor_modal_window_lib"
+/*
+mfLib({
+    catalog: "collaborators",
+    fields: "id,fullname,code",
+    find: "id,fullname,code",
+    value: "Тулинова",
+    ids: "",
+    'user-where': "1=1",
+    ssql: "",
+    connection: "",
+}).then(data => console.log("data", data))
+*/
