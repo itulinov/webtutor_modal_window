43d1f0d15b808bd203b1f446aba0965e27b6e0c5
iTulinov <energiay@gmail.com>
2024-03-20 11:46:28 +0300

    автоФокус и поиск по enter

--- src/App/ModalWindow/Search/Input/Input.js
+++ src/App/ModalWindow/Search/Input/Input.js
@@ -4,13 +4,22 @@ import Classes from "./Input.module.css"
 
 function Input({getRecords}) {
     const [value, setValue] = useState("")
+    const onEnter = ({target, key}) => {
+        if (key !== "Enter") {
+            return
+        }
+
+        getRecords(target.value)
+    }
 
     return (
         <div className={Classes.input}>
             <input type="text"
+                autoFocus
                 placeholder="Поиск"
                 onInput={({target}) => setValue(target.value)}
                 defaultValue={value}
+                onKeyDown={onEnter}
             />
             <div className={Classes.btn} onClick={() => getRecords(value)}>
                 <img src={search} alt="go" />
